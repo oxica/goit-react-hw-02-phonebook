6b9629e0ca8a2b4be224e690abf07baeef7d2f64
Oksana <kskaplun@gmail.com>
2022-05-08 23:28:17 +0300

    01

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,55 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        textTransform: 'uppercase',
-        color: '#010101',
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+import ContactForm from './ContactForm/ContactForm';
+import { nanoid } from 'nanoid';
+
+// import s from './App.module.css';
+
+class App extends Component {
+  state = {
+    contacts: [],
+    name: '',
+  };
+
+  addContact = ({ name, number }) => {
+    let isAdded = false;
+    this.state.contacts.forEach(el => {
+      if (el.name === name) {
+        alert(`${name} is already in contacts`);
+        isAdded = true;
+      }
+    });
+    if (isAdded) {
+      return;
+    }
+    const contact = {
+      id: nanoid(),
+      name: name,
+      number: number,
+    };
+    this.setState(({ contacts }) => ({
+      contacts: [contact, ...contacts],
+    }));
+  };
+
+  render() {
+    return (
+      <div
+        style={{
+          height: '100vh',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          flexDirection: 'column',
+          fontSize: 18,
+          textTransform: 'uppercase',
+          color: '#010101',
+        }}
+      >
+        <h1>Phonebook</h1>
+        <ContactForm onSubmit={this.addContact} />
+      </div>
+    );
+  }
+}
+
+export default App;
